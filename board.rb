98ecd6a2b852acd2572f44a62799627fa11fc0b6
Peter <sb1285n@gmail.com>
2019-09-01 15:13:02 -0400

    Created calculate_adjacent_bombs method on Board class. Started to distinguish between what a tile value is and what is rendered to the board

--- board.rb
+++ board.rb
@@ -3,7 +3,7 @@ require_relative "tile"
 class Board
   def self.from_file(filename)
     rows = File.readlines(filename).map(&:chomp)
-    tiles = rows.map { |row| row.split("").map { |tile| Tile.new(tile, rows) } }
+    tiles = rows.map { |row| row.split("").map { |tile| Tile.new(tile) } }
 
     Board.new(tiles)
   end
@@ -17,12 +17,39 @@ class Board
   def render
     puts "\e[H\e[2J"
     puts "    " + @grid.map.with_index { |_, i| i.to_s }.join(" ")
+    puts ""
+
     grid.each_with_index do |row, i|
       display_row = i.to_s + " | "
-      row.each { |tile| display_row += tile.get_val + " " }
+      row.each_with_index { |_, j| display_row += calculate_adjacent_bombs(i, j) + " " }
       puts display_row
     end
+
     puts ""
+  end
+
+  def reveal(pos = [0, 1])
+    x, y = pos
+    grid[x][y].revealed = true
+    render
+  end
+
+  def calculate_adjacent_bombs(row, column)
+    tile = grid[row][column]
+    return tile.render unless tile.render == "_"
+
+    count = 0
+
+    (row-1..row+1).each do |i|
+      next unless grid[i] 
+      (column-1..column+1).each do |j|
+        adjacent_tile = grid[i][j]
+        next unless adjacent_tile
+        count += 1 if adjacent_tile.get_val == "b"
+      end
+    end
+
+    count == 0 ? tile.render : count.to_s
   end
 
   private
@@ -30,3 +57,6 @@ class Board
 end
 
 board = Board.from_file("./grids/minefield1.txt")
+board.reveal
+board.reveal([5, 5])
+board.reveal([3, 7])
--- tile.rb
+++ tile.rb
@@ -1,15 +1,23 @@
 class Tile
-  def initialize(initial_value, grid)
-    @grid = grid
-    @value = set_value(initial_value)
-    @revealed = true
+  attr_accessor :revealed
+
+  def initialize(val)
+    @value = val
+    @revealed = false
+  end
+
+  def render
+    @revealed ? value : "*"
   end
 
   def get_val
-    @revealed ? @value : "*"
-  end
-
-  def set_value(value)
     value
   end
+
+  def set_val(val)
+    value = val
+  end
+
+  private
+  attr_accessor :value
 end
