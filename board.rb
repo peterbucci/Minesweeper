d7342d69af1513d94f17af503317a1820b98299a
Peter <sb1285n@gmail.com>
2019-09-06 15:22:04 -0400

    Moved game_over logic from the Board to the Game class. Finished the leaderboard so it will only add you if you win. Refactored Game a bit.

--- board.rb
+++ board.rb
@@ -1,25 +1,24 @@
 require_relative "tile"
 
 class Board
+  attr_reader :lose
+
   def self.create(rows, bombs)
-    count = rows.join("").length
     tiles = rows.each_with_index.map { |row, i| row.split("").each_with_index.map { |tile, j| Tile.new(tile, i, j) } }
 
-    Board.new(tiles, count, bombs)
+    Board.new(tiles, bombs)
   end
 
-  def initialize(grid, safe_squares, bombs)
+  def initialize(grid, bombs)
     @grid = grid
     @bombs_to_add = bombs
-    @bombs_left = bombs
-
-    @safe_squares_remaining = safe_squares
-    @lose = false
+    @bombs_left_to_flag = bombs
+    @safe_squares_revealed = 0
   end
 
-  def render(turn, timer, player, game_over = false)
+  def render(timer, player, game_over = false)
     puts "\e[H\e[2J"
-    puts "Player: " + player + " | Turns: " + turn.to_s + " | Bombs: " + @bombs_left.to_s + " | Time: " + timer.to_s
+    puts "Player: " + player + " | Bombs: " + @bombs_left_to_flag.to_s + " | Time: " + timer.to_s
     puts "\n     " + @grid.map.with_index { |_, i| i < 10 ? i.to_s + " " : i.to_s }.join(" ") + "\n\n"
 
     grid.each_with_index do |row, i|
@@ -37,7 +36,7 @@ class Board
     out_of_bounds = (row == -1 || column == -1)
     return if out_of_bounds || current_tile.revealed
   
-    @bombs_left += 1 if current_tile.render == "F"
+    @bombs_left_to_flag += 1 if current_tile.render == "F"
     current_tile.reveal && check_for_bomb(current_tile)
 
     get_adjacent_tiles(current_tile).each { |tile| reveal(tile, turn) } if current_tile.get_val == "_"
@@ -46,36 +45,19 @@ class Board
   def toggle_flag(tile)
     if tile.flag
       tile.flag = false
-      @bombs_left += 1
+      @bombs_left_to_flag += 1
     else
       tile.flag = true
-      @bombs_left -= 1
+      @bombs_left_to_flag -= 1
     end
+  end
+
+  def safe_squares_remaining?(safe_squares)
+    safe_squares - @safe_squares_revealed
   end
 
   def check_for_bomb(current_tile)
-    if current_tile.get_val == "b"
-      @lose = true
-      current_tile.set_val("X")
-    else
-      @safe_squares_remaining -= 1
-    end
-  end
-
-  def game_over?
-    win = @safe_squares_remaining == 0
-
-    win || lose
-  end
-
-  def game_over_message(turn)
-    if lose 
-      puts "Sorry, you lose!"
-      puts "You Lost in " + turn.to_s + " turns."
-    else
-      puts "Congratulations! You win!" 
-      puts "You won in " + turn.to_s + " turns."
-    end
+    current_tile.get_val == "b" ? current_tile.set_val("X") : @safe_squares_revealed += 1
   end
 
   def add_bombs(current_tile)
@@ -85,7 +67,6 @@ class Board
     random_tile.set_val("b")
 
     @bombs_to_add -= 1
-    @safe_squares_remaining -= 1
   
     add_bombs(current_tile)
   end
@@ -133,5 +114,5 @@ class Board
   end
 
   private
-  attr_reader :grid, :lose
+  attr_reader :grid
 end
--- game.rb
+++ game.rb
@@ -2,16 +2,17 @@ require_relative "board"
 
 class Game
   def self.configure
-    levels = { "b" => [9, 10], "i" => [16, 40], "e" => [22, 60] }
+    levels = { "beginner" => [9, 10], "intermediate" => [16, 40], "expert" => [22, 60] }
 
-    player_name = Game.set_option("What's your name?", /^[a-zA-Z]{1,5}$/)
-    difficulty = Game.set_option("Choose a difficulty |b|eginner, |i|ntermediate, or |e|xpert", /b|i|e/)
+    player_name = Game.set_option("What's your name?", /^[a-zA-Z]{1,8}$/)
+    difficulty = Game.set_option("Choose a difficulty |beginner|, |intermediate|, or |expert|", /beginner|intermediate|expert/)
     size = levels[difficulty][0]
     bombs = levels[difficulty][1]
+    safe_squares = (size * size) - bombs
 
     created_board = Game.create_a_board(size, bombs)
 
-    Game.new(player_name, created_board, size, difficulty)
+    Game.new(player_name, created_board, size, difficulty, safe_squares)
   end
 
   def self.set_option(message, pattern, value = nil)
@@ -19,7 +20,7 @@ class Game
 
     puts "\n" + message
 
-    value = gets.chomp.downcase
+    value = gets.chomp
     Game.set_option(message, pattern, value)
   end
 
@@ -28,27 +29,32 @@ class Game
     Board.create(rows, bombs)
   end
 
-  def initialize(player_name, board, size, difficulty)
+  def initialize(player_name, board, size, difficulty, safe_squares)
     @board = board
     @size = size
     @difficulty = difficulty
     @start_time = Time.now.to_i
     @turn = 0
     @player = player_name.capitalize
+    @safe_squares = safe_squares
+    @flags_placed = 0
+    @lose = false
 
     run
   end
   
   def run
-    until board.game_over?
+    until game_over?
       current_time = Time.now.to_i
-      board.render(@turn, current_time - start_time, @player)
+      board.render(current_time - start_time, @player)
       selected_pos = board[get_pos]
       if @flag 
+        @flags_placed += 1
         board.toggle_flag(selected_pos)
       else
         @turn += 1
         board.reveal(selected_pos, @turn)
+        @lose = true if selected_pos.get_val == "X"
       end
     end
 
@@ -80,35 +86,61 @@ class Game
     pos.split(",").map(&:to_i)
   end
 
+  def game_over?
+    win = board.safe_squares_remaining?(@safe_squares) == 0
+
+    win || @lose
+  end
+
   def end_game
     final_time = Time.now.to_i - start_time
-    board.render(@turn, final_time, @player, true)
+    board.render(final_time, @player, true)
     puts "\n"
-    board.game_over_message(@turn)
-    add_to_leaderboard(final_time)
+    if @lose 
+      puts "Sorry, you lose!"
+      puts "You Lost in #{@turn.to_s} turns."
+    else
+      puts "Congratulations! You win!" 
+      puts "You won in #{@turn.to_s} turns."
+    end
+    @flags_placed == 1 ? n = "#{@flags_placed.to_s} tile" : n = "#{@flags_placed.to_s} tiles"
+    puts "You marked #{n} with a flag."
+
+    add_to_leaderboard(final_time) unless @lose
     retrieve_leaderboard
   end
 
   def retrieve_leaderboard
-    puts "\n" + "Top Scores"
+    puts "\n" + "Top Times - #{@difficulty.capitalize}"
     puts "\n"
     File.open("./leaderboards/#{@difficulty}.txt", "r").each { |line| puts "#{$.}.  " + line.chomp }
   end
 
   def add_to_leaderboard(time)
     file = "./leaderboards/#{@difficulty}.txt"
-    current_score = @player.to_s + " " + time.to_s
-    leaderboard = [current_score]
+    leaderboard = []
 
     File.readlines(file).each { |top_score| leaderboard << top_score.chomp }
 
-    leaderboard.sort! do |x, y|
-      first_time = x.split(" ")[1].to_i
-      second_time = y.split(" ")[1].to_i
-      first_time <=> second_time
-    end
+    worst_time = leaderboard[-1].split(" ")[1]
 
-    leaderboard.pop if leaderboard.length > 10
+    if worst_time.to_i > time || leaderboard.length < 10
+      current_time = @player.to_s + " " + time.to_s
+      leaderboard << current_time
+
+      leaderboard.sort! do |x, y|
+        first_time = x.split(" ")[1].to_i
+        second_time = y.split(" ")[1].to_i
+        first_time <=> second_time
+      end
+
+      leaderboard.pop if leaderboard.length > 10
+
+      puts "\n" + "You made it on the leaderboard!"
+    else
+      puts "\n" + "You didn't make it on the leaderboard."
+      puts "Try again!"
+    end
 
     File.open(file, "w") { |file| file.puts leaderboard }
   end
